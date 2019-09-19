dfb4d98c39172dbfef501abf22adb2132128650c
WeiZhangInterview <55388499+WeiZhangInterview@users.noreply.github.com>
2019-09-19 11:57:35 +0800

    test deploy server password

--- db/db.go
+++ db/db.go
@@ -9,7 +9,7 @@ var CONN *sql.DB
 func init() {
 	var err error
 	//CONN, err = sql.Open("mysql", "root:12341234@tcp(127.0.0.1:3306)/order_system?parseTime=true")
-	CONN, err := sql.Open("mysql", "order_system:wei_zhang@tcp(db:3306)/order_system")
+	CONN, err := sql.Open("mysql", "root:root@tcp(db:3306)/order_system")
 	//NOTE: Avoid Tmie Wait MYSQL CONN
 	CONN.SetMaxOpenConns(20)
 	CONN.SetMaxIdleConns(20)
